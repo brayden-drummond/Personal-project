cbdf7f0f5e07d88d26448f586a27044dd1b9ac4c
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-08-29 08:44:41 +1200

    homepage container 2 positioning

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,7 +10,30 @@ function Home() {
         ></img>
       </div>
       <div className="home-text-container">
-        <div className="home-text"></div>
+        <div className="home-text-1">
+          <div>
+            <h2>Follow me</h2>
+            <p>Text</p>
+          </div>
+        </div>
+        <div className="home-text-2">
+          <div>
+            <h2>Bungie website</h2>
+            <p>Text</p>
+          </div>
+        </div>
+        <div className="home-text-3">
+          <div>
+            <h2>Gameplay videos</h2>
+            <p>Text</p>
+          </div>
+        </div>
+        <div className="home-text-4">
+          <div>
+            <h2>Other Games</h2>
+            <p>Text</p>
+          </div>
+        </div>
       </div>
     </div>
   )
