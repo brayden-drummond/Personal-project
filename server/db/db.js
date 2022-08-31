fef3cf29cd3ae3e6a04816308212bca04de3007c
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-01 11:59:16 +1200

    setting up database and routes

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,11 @@
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
+
+module.exports = {
+  getDlc,
+}
+
+function getDlc(db = connection) {
+  return db('destiny2').select()
+}
