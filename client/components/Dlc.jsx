d242f438f9d3dad1188bcded249b6594b91aba59
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-04 11:11:32 +1200

    dlc page start finished

--- client/components/Dlc.jsx
+++ client/components/Dlc.jsx
@@ -9,19 +9,23 @@ function Dlc() {
 
   return (
     <div className="blog-container">
-      <div className="blog-header">{dlc.dlcName}</div>
-      <img className="blog-image" src={`/images/${dlc.image}`} />
-      <div>
-        <h3>About</h3>
-        <div className="blog-about">{dlc.about}</div>
-      </div>
-      <div>
-        <h3>Activities</h3>
-        <div className="blog-activities">{dlc.activities}</div>
-      </div>
-      <div>
-        <h3>Reviews</h3>
-        <div className="blog-review">{dlc.review}</div>
+      <div className="blog-box">
+        <div className="blog-header">
+          <h2>{dlc.dlcName}</h2>
+        </div>
+        <img className="blog-image" src={`/images/${dlc.image}`} />
+        <div>
+          <h3>About</h3>
+          <div className="blog-about">{dlc.about}</div>
+        </div>
+        <div>
+          <h3>Activities</h3>
+          <div className="blog-activities">{dlc.activities}</div>
+        </div>
+        <div>
+          <h3>Reviews</h3>
+          <div className="blog-review">{dlc.review}</div>
+        </div>
       </div>
     </div>
   )
